8140ac4f56ac773460750de4e2c2bb59cd842a13
jaehyuen <violet6520@naver.com>
2021-08-21 19:28:00 +0900

    [2021/08/21 이재현] webflux 스터디
    1. thymeleaf 적용

--- webflux-study/src/main/java/com/study/webflux/controller/ServerController.java
+++ webflux-study/src/main/java/com/study/webflux/controller/ServerController.java
@@ -12,14 +12,18 @@ import reactor.core.publisher.Flux;
 
 @RestController
 @RequiredArgsConstructor
-public class ServcerController {
+public class ServerController {
 
 	 private final KitchenSerivce kitchen;
 	 
 	 @GetMapping(value = "server",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
 	 public Flux<Dish> serveDishes(){
-		 return this.kitchen.getDishes();
+		 return kitchen.getDishes();
 	 }
-	
+
+	 @GetMapping(value = "served-dishes",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	 public Flux<Dish> deliverdDishes(){
+		return kitchen.getDishes().map(dish -> Dish.deliver(dish)); 
+	 }
 	
 }
