d061481f4c69c854772d1495a7ae220c3bb979aa
jaehyuen <violet6520@naver.com>
2021-09-12 20:52:41 +0900

    [2021/09/12 이재현] 평문형 쿼리 예쩨 (p103 ~ p105)

--- webflux-study/src/main/java/com/study/webflux/service/InventoryService.java
+++ webflux-study/src/main/java/com/study/webflux/service/InventoryService.java
@@ -16,6 +16,10 @@
 
 package com.study.webflux.service;
 
+import static org.springframework.data.mongodb.core.query.Criteria.byExample;
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
+
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.ExampleMatcher.StringMatcher;
@@ -33,6 +37,7 @@ import reactor.core.publisher.Flux;
 public class InventoryService {
 
 	private final ItemRepository repository;
+	private final ReactiveFluentMongoOperations fluentOperations;
 
 	public Flux<Item> searchByExample(String name, String description, boolean useAnd) {
 		Item item = new Item(name, description, 0.0);
@@ -43,6 +48,27 @@ public class InventoryService {
 		Example<Item> probe = Example.of(item, matcher);
 
 		return repository.findAll(probe);
+	}
+
+	public Flux<Item> searchByFluentExample(String name, String description) {
+		return fluentOperations.query(Item.class) //
+				.matching(query(where("TV tray").is(name).and("Smurf").is(description))) //
+				.all();
+	}
+
+	public Flux<Item> searchByFluentExample(String name, String description, boolean useAnd) {
+		Item item = new Item(name, description, 0.0);
+
+		ExampleMatcher matcher = (useAnd //
+				? ExampleMatcher.matchingAll() //
+				: ExampleMatcher.matchingAny()) //
+						.withStringMatcher(StringMatcher.CONTAINING) //
+						.withIgnoreCase() //
+						.withIgnorePaths("price");
+
+		return fluentOperations.query(Item.class) //
+				.matching(query(byExample(Example.of(item, matcher)))) //
+				.all();
 	}
 
 }
